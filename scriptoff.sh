bda0f903f5b6e14e2557305def8e110604717c44
kimsufi <root@ks394808.kimsufi.com>
2015-03-03 13:15:41 +0100

    bug du script chmod

--- scriptoff.sh
+++ scriptoff.sh
@@ -4,7 +4,7 @@ SERVERNAME=/home/web/iconeo.fr
 
 chown -R www-data.ftpgroup $SERVERNAME
 chmod -R 555 $SERVERNAME
-chmod -R 555 $SERVERNAME/.*
+chmod -R 555 $SERVERNAME/.htaccess
 chmod -R 777 $SERVERNAME/admin/FileUpload/server/php/files
 chmod -R 755 $SERVERNAME/uploads
 chmod -R 755 $SERVERNAME/photos
