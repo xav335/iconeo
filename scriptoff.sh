19859ac6096e466cfc091f12e66ae4df5b4743a6
Xavier Gonzalez <xavier@gonzalez.pm>
2015-04-09 17:49:54 +0200

    mail desinscription + log

--- scriptoff.sh
+++ scriptoff.sh
@@ -6,5 +6,6 @@ chown -R www-data.ftpgroup $SERVERNAME
 chmod -R 555 $SERVERNAME
 chmod -R 555 $SERVERNAME/.htaccess
 chmod -R 777 $SERVERNAME/admin/FileUpload/server/php/files
+chmod 755 $SERVERNAME/admin/spy.log
 chmod -R 755 $SERVERNAME/uploads
 chmod -R 755 $SERVERNAME/photos
